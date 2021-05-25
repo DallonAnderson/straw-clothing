fa80b00cefe440024ad63797185dd70a4fd8cc82
Dallon Anderson <dallon.anderson@gmail.com>
2021-05-25 11:53:25 -0400

    custom form inputs and button added, sign in component updated

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import "./App.css";
 import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
